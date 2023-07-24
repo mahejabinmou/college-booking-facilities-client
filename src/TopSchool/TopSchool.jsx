70dddf5454c0d1c18fd6411a03ff826ce75d904b
Mou <mahejabinmou.99@gmail.com>
2023-07-24 10:14:39 -0700

    top college extra section edit

--- src/TopSchool/TopSchool.jsx
+++ src/TopSchool/TopSchool.jsx
@@ -2,17 +2,16 @@
 
 const TopSchool = () => {
     return (
-        <div>
-            <h1 className="font-bold text-center text-success p-2">First Position achiver college in hsc 2023</h1>
-             <div className="grid grid-rows-2  gap-4   ">
-
-            <img src="https://i.ibb.co/hKvzD7n/rajuk.jpg" alt="" />
-            
-            <p className=""><small>First Top 10 College in Bangladesh
+        <div className="p-8">
+            <h1 className="font-bold text-center text-success">First Position achiver college in hsc 2023</h1>
+            <h3 className="font-bold text-center text-success">Rajuk uttara model college</h3>
+            <div className="grid md:grid-cols-2 gap-4 p-4  ">
+            <p className="p-8"><small>First Top 10 College in Bangladesh
             Rajuk Uttara Model College scored the highest 
             98.04 points under the ranking system of the 
             inter-education board standard – which is based 
             on the students, pass rate, and GPA 5 score.</small></p>
+            <img src="https://i.ibb.co/hKvzD7n/rajuk.jpg" alt="" />
             </div>
         </div>
     );
